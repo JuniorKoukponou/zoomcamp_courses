b5e06ef423c9e4c043d21322e41e488102d0b795
JuniorKoukponou <30500212+JuniorKoukponou@users.noreply.github.com>
2024-06-12 14:14:08 +0000

    adding docker file

--- mlops/04-deployment/web-service/predict_flask.py
+++ mlops/04-deployment/web-service/predict_flask.py
@@ -18,5 +18,20 @@ def predict(features):
     prediction = model.predict(X)
     return prediction[0]
 
+app = Flask("duration-prediction")
 
+@app.route("/predict", methods=["POST"])
 def predict_endpoint():
+    """take web request and return the prediction"""
+    ride = request.get_json()
+    features = prepare_features(ride)
+    preds = predict(features)
+    result = {
+        "duration": preds
+    }
+
+    return jsonify(result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
