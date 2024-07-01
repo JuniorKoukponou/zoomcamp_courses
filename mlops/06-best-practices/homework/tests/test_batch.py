7014caab517e7e98cd2e9a625917de7393f3c6a4
JuniorKoukponou <30500212+JuniorKoukponou@users.noreply.github.com>
2024-07-01 22:48:02 +0000

    init homework

---
+++ mlops/06-best-practices/homework/tests/test_batch.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import batch
+from datetime import datetime
+from deepdiff import DeepDiff
+
+
+def dt(hour, minute, second=0):
+    return datetime(2023, 1, 1, hour, minute, second)
+
+def test_prepare_data():
+    data = [
+        (None, None, dt(1, 1), dt(1, 10)),
+        (1, 1, dt(1, 2), dt(1, 10)),
+        (1, None, dt(1, 2, 0), dt(1, 2, 59)),
+        (3, 4, dt(1, 2, 0), dt(2, 2, 1)),      
+    ]
+
+    columns = ['PULocationID', 'DOLocationID', 'tpep_pickup_datetime', 'tpep_dropoff_datetime']
+    df = pd.DataFrame(data, columns=columns)
+
+    actual_prepared_data = batch.prepare_data(df=df, categorical=columns)
+    print("actual_prepared_data", actual_prepared_data)
+    expected_prepared_data = pd.DataFrame(
+        [
+            (-1, -1, dt(1, 1), dt(1, 10), 9),
+            (1, 1, dt(1, 2), dt(1, 10), 8)
+            # (1, -1, dt(1, 2, 0), dt(1, 2, 59), 59 / 60),
+            # (3, 4, dt(1, 2, 0), dt(2, 2, 1), 1 / 60),      
+        ],
+        columns=columns + ["duration"])
+
+    diff = DeepDiff(actual_prepared_data, expected_prepared_data, significant_digits=1)
+    print(f'diff={diff}')
+
+    assert 'values_changed' not in diff
+    assert 'type_changes' not in diff
